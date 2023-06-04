0b8cd0cbbee1690257c805f17fffbd0ca7793b73
David Gregory <42326109+hunoz@users.noreply.github.com>
2023-06-04 17:12:28 -0500

    Added dropdown for multiselect album filtering and columns are now sorted by album title for increased findability

--- src/types/Setlist.ts
+++ src/types/Setlist.ts
@@ -1,14 +1,10 @@
 import { Track } from './Track';
 
-export interface Setlist {
-    "debut": Track[],
-    "fearless": Track[],
-    "speakNow": Track[],
-    "red": Track[],
-    "1989": Track[],
-    "reputation": Track[],
-    "lover": Track[], 
-    "folklore": Track[],
-    "evermore": Track[],
-    "midnights": Track[],
-}
+export interface SetlistData {
+    albumName: string;
+    columnNumber: number;
+    setList: Track[];
+    colorData: string;
+    headerCssStyle: {[key: string]: string};
+    dataViewClassName?: string;
+};
